f82dfe8c1506e8826ae1addf82353c2c4509129c
Benjamin Ran <benjaminran2@gmail.com>
2017-03-29 00:23:36 -0700

    scrape index.htm (not index1.htm)

--- standards-list/scrape.sh
+++ standards-list/scrape.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 outfile=$1
-for i in {1..10}; do
+curl "http://www.jazzstandards.com/compositions/index.htm" | grep '<tr' | grep '<a' | sed 's/.*<a.*>\(.*\)<\/a>.*/\1/'
+for i in {2..10}; do
     curl "http://www.jazzstandards.com/compositions/index${i}.htm" | grep '<tr' | grep '<a' | sed 's/.*<a.*>\(.*\)<\/a>.*/\1/'
 done
